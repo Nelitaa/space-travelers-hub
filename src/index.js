499ec9ce396cce6690143e14d9a1c50761743250
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-20 11:16:20 -0400

    Add Provider to the store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Provider store={store}>
       <App />
-    </Router>
+    </Provider>
   </React.StrictMode>,
 );
