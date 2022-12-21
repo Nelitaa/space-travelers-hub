2b4af9790bec6f8f3d210a15e65224541d8c602f
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2022-12-21 14:49:43 -0400

    Add rockets reserved section on My profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,23 @@
-const Profile = () => (
-  <div className="profile-container">Profile</div>
-);
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+
+  return (
+    <div className="profile-container">
+      <section>
+        <h1>My Rockets</h1>
+        <ul>
+          {rockets.filter((rocket) => rocket.rocket_reserved).map((rocket) => (
+            <li key={rocket.rocket_id}>
+              {rocket.rocket_name}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default Profile;
